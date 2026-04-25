53d63f663ef270549af7d2669d3f92d55c37350b
lukea <lukea@DESKTOP-8NA7JFL>
2026-04-25 15:37:06 +1200

    Initial Creation of the project

---
+++ PDC_Project1/src/pdc_project1/PDC_Project1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pdc_project1;
+
+/**
+ *
+ * @author lukea
+ */
+public class PDC_Project1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
